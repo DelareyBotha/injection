00697988009d07e2f8b253e406e75894334038c6
delarey <dbotha95@gmail.com>
2021-06-08 14:35:28 +0200

    added new project structure mimicking spring boot's structure of using repositories, controllers, services and entities added initial code for the user entity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Some Text");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req:any, res:any)=>{
+    res.status("200").json({
+        message: "This is the root directory!"
+    });
+});
+
+app.get("/stuff", (req: any, res: any)=>{
+    res.status("200").json({
+       message: "Hello from express"
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on ${port}`);
+});
